b77e2ad3a8c6866cebd030e55a1cbfc38129b3e0
pranavi.kedari@gmail.com <praneet.kedari@gmail.com>
2022-12-28 18:06:19 -0500

    changed header

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import Head from "next/head";
+import Image from "next/image";
+import buildspaceLogo from "../assets/buildspace-logo.png";
 
 const Home = () => {
   return (
@@ -11,7 +11,7 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>hi</h1>
           </div>
           <div className="header-subtitle">
             <h2>insert your subtitle here</h2>
